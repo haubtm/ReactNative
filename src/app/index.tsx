fdc72a343214fac9c970b617aefdd37e5fc056da
ADMIN-PC\Admin <haubtm699@gmail.com>
2025-03-07 18:13:43 +0700

    55

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const AppRoot = () => {
+    return (
+        <View>
+            <Text>Hello World</Text>
+        </View>
+    );
+}
+
+export default AppRoot;
